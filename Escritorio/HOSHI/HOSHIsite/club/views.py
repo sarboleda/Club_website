2f677db8ea0d623a14a22d85beef4157b1dcf36f
sarboledab <sebinn49@gmail.com>
2019-12-23 15:04:28 -0500

    Cambie el home

--- Escritorio/HOSHI/HOSHIsite/club/views.py
+++ Escritorio/HOSHI/HOSHIsite/club/views.py
@@ -5,3 +5,8 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'club/index.html', {})
 
+def home(request):
+    return render(request, 'club/home.html', {})
+
+
+
